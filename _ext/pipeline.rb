d39655c02f3d00fcb07f95c190ad0d6c569d065f
Lance Ball <lball@redhat.com>
2012-05-03 17:28:19 -0400

    Namespace the yardoc extension.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,9 +1,9 @@
-require 'yard'
+require File.join(File.dirname(__FILE__), 'yard')
 require 'extension_documenter'
 
 Awestruct::Extensions::Pipeline.new do
   extension ExtensionDocumenter.new
-  extension Yard.new
+  extension Awestruct::Extensions::Yard.new
   extension Awestruct::Extensions::Posts.new( '/news' )
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom' )
